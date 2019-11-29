0bf1693abf95c27d69c47d28729809833df47d5e
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2019-11-29 14:03:30 +0200

    UPDATE -> URL to PDF

--- Puppeteer/webPage-to-PDF.js
+++ Puppeteer/webPage-to-PDF.js
@@ -4,9 +4,9 @@ class Webpage {
     static async generatePDF(url) {
         const browser = await puppeteer.launch({ headless: true }); // Puppeteer can only generate pdf in headless mode.
         const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'networkidle', networkIdleTimeout: 5000 }); // Adjust network idle as required. 
+        await page.goto(url);
         const pdfConfig = {
-            path: 'url.pdf', // Saves pdf to disk. 
+            path: '<filename>.pdf', // Saves pdf to disk. 
             format: 'A4',
             printBackground: true,
             margin: { // Word's default A4 margins
@@ -26,6 +26,6 @@ class Webpage {
 }
 
 (async() => {
-    const url = 'https://ia601405.us.archive.org/18/items/alicesadventures19033gut/19033-h/19033-h.htm';
+    const url = '<pdf-url>';
     const buffer = await Webpage.generatePDF(url);
 })();
